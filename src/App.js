64d523f703612c2d6ffacafc35f6058f44a07789
Andrew Thompson <111794782+Andrewtho5942@users.noreply.github.com>
2024-01-01 16:08:32 -0600

    implement RCR feedback

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ export default function App() {
 
   return (
     <div className = "bg-gray-500 h-screen"> 
-        <iframe title="River City Reborn Game Window" className="mx-auto pt-2" width="1600" height="800" src="RCR/index.html"/>
+        <iframe title="River City Reborn Game Window" className="" width="1600" height="800" src="RCR/index.html"/>
     </div>
 
   );
